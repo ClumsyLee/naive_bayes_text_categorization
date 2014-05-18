93bdadd99a66fa0216df116a75d0b1064d1469f6
ThomasLee911 <lisihan969@gmail.com>
2014-05-19 01:13:00 +0800

    new test method

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,20 @@
 #include <iostream>
 #include "naive_bayes.h"
 #include "filter.h"
+#include "test_naive_bayes.h"
 
 using namespace homework::experiment;  // for convenience
 
-void InteractiveLearn(NaiveBayes &classifier);
-void InteractiveClassify(const NaiveBayes &classifier);
-
-int main()
+int main(int argc, char *argv[])
 {
-    NaiveBayes classifier(new BasicFilter);
-
-    while (true)
+    if (argc != 2)
     {
-        InteractiveLearn(classifier);
-        InteractiveClassify(classifier);
+        std::cerr << "Usage: " << argv[0] << " <training dir>\n";
+        std::exit(EXIT_FAILURE);
     }
+
+    TestNaiveBayes tester(argv[1]);
+    tester.CrossValidate();
 
     return 0;
-}
-
-void InteractiveLearn(NaiveBayes &classifier)
-{
-    std::vector<NaiveBayes::File> files;
-
-    std::string category_name;
-    std::cout << "Please enter the category: ";
-    std::getline(std::cin, category_name);
-
-    std::cout << "Please enter file names (Category: " << category_name << ")\n"
-                 "Enter an empty line to stop:\n";
-
-    std::string filename;
-    while (std::getline(std::cin, filename) && !filename.empty())
-    {
-        files.push_back({filename, category_name});
-    }
-
-    std::cout << classifier.Learn(files) << " file(s) learned\n";
-}
-
-void InteractiveClassify(const NaiveBayes &classifier)
-{
-    std::string filename;
-    std::cout << "Please enter file name: ";
-
-    while (std::getline(std::cin, filename) && !filename.empty())
-    {
-        const std::string *category_name = classifier.Classify(filename, true);
-        if (category_name)
-        {
-            std::cout << "Classify to " << *category_name << std::endl;
-        }
-        else
-        {
-            std::cout << "Classification failed\n";
-        }
-    }
 }
